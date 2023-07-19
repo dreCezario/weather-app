88479b8a8a3713ed37619b8f73a2cf10dd8e18bb
André Cezario <developer.andrecezario@gmail.com>
2023-07-19 17:35:43 -0300

    definindo layout do App

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
 import { useState } from 'react'
 import './App.css'
+import search from './assets/icons/search.svg'
 
 function App() {
 
+  const [input, setInput] = useState('')
+
   return (
-    <div className='text-xl font-bold text-center'>
-      hello world
+    <div className='w-full h-screen text-white px-8'>
+
+      <nav className='w-full p-3 flex justify-between items-center'>
+
+        <h1 className='font-bold tracking-wide text-3xl'>App Clima</h1>
+
+        <div className='bg-white w-[15rem] overflow-hidden shadow-2xl rounded flex items-center p-2 gap-2'>
+          <img src={search} alt="search" className='w-[1.5rem] h-[1.5rem]' />
+
+          <input onKeyUp={(e) => {
+            if(e.key === 'Enter') {
+              // sumit the form
+            }
+          }} type="text" className='focus:outline-none w-full text-[#212121] text-lg' value={input} onChange={ e => setInput(e.target.value)} />
+        </div>
+      </nav>
     </div>
   )
 }
