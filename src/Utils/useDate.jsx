ce6283aa4eecb07492cb96b2aa4c86f22cb1cb1c
André Cezario <developer.andrecezario@gmail.com>
2023-07-19 17:16:30 -0300

    definindo useDate hook

--- src/Utils/useDate.jsx
+++ src/Utils/useDate.jsx
@@ -11,5 +11,19 @@ export const useDate = () => {
             setDate(new Date())
         }, 60*1000)
 
+        return () => {
+            clearInterval(timer)
+        }
     }, [])
+
+    const day = today.toLocaleDateString(locale, {weekday: 'long'})
+
+    const date =  `${day}, ${today.getDate()}, ${today.toLocaleDateString(locale, {month: 'long'})}\n\n`
+
+    const time = today.toLocaleDateString(locale, {hour: 'numeric', hour12: false, minute: 'numeric'})
+
+    return {
+        date,
+        time
+    }
 }
